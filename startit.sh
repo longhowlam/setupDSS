9ea75c9a9154c1b6b8d5bd14d5916f196c4fc553
longhow lam <longhowlam@gmail.com>
2017-11-16 14:44:17 +0100

    Update startit.sh

--- startit.sh
+++ startit.sh
@@ -11,6 +11,11 @@ gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install r-base
 
+### install RStudio
+sudo apt-get -y install libapparmor1 gdebi-core
+wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
+sudo apt-get -y install rstudio-server-1.1.383-amd64.deb
+
 ### install packages
 # installeer dependencies om devtools te kunnen installeren
 sudo apt-get -y install libcurl4-gnutls-dev
@@ -18,8 +23,3 @@ sudo apt-get -y install libxml2-dev
 sudo apt-get -y install libssl-dev
 # packages
 sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\""
-
-### install RStudio
-sudo apt-get -y install libapparmor1 gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
-sudo apt-get -y install rstudio-server-1.1.383-amd64.deb
