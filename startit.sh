15c998b5577b63f036c02b0fe335529461369a28
longhow lam <longhowlam@gmail.com>
2017-11-16 16:46:20 +0100

    Update startit.sh

--- startit.sh
+++ startit.sh
@@ -26,9 +26,11 @@ cd /home/ruser/
 wget https://downloads.dataiku.com/public/studio/4.1.0/dataiku-dss-4.1.0.tar.gz
 tar xzf /home/ruser/dataiku-dss-4.1.0.tar.gz
 
-### needs to be installed not as root
-sudo -u ruser -i "/home/ruser/dataiku-dss-4.1.0/scripts/install/install-deps.sh"
-sudo -u ruser '/home/ruser/dataiku-dss-4.1.0/installer.sh -d dataikudir -p 11000'
+### install dependencies first 
+/home/ruser/dataiku-dss-4.1.0/scripts/install/install-deps.sh -yes
+### needs to be installed not as root, and set access to a dir that was created
+sudo chmod -R 777 /home/ruser/dataiku-dss-4.1.0/
+sudo -u ruser /home/ruser/dataiku-dss-4.1.0/installer.sh -d dataikudir -p 11000
 
 ### Start DSS
 /home/dataiku/dss_data/bin/dss start
