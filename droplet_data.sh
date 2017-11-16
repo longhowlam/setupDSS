150ddaab0fbb84d6af2c1afd5811934a292686e4
longhow lam <longhowlam@gmail.com>
2017-11-16 15:37:38 +0100

    Update droplet_data.sh

--- droplet_data.sh
+++ droplet_data.sh
@@ -1,4 +1,14 @@
 #cloud-config
+users:
+  - name: ruser
+    groups: sudo
+    shell: /bin/bash
+    
+chpasswd:
+  list: |
+    ruser:ruser123
+  expire: False
+
 runcmd:
   - [ git , clone , 'https://github.com/longhowlam/setupDSS.git']
   - echo "git cloned"
