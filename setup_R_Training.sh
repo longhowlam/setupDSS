494090fd014e4b4b59e2bd99b084a1a836410088
longhow lam <longhowlam@gmail.com>
2018-05-11 12:07:44 +0200

    Add files via upload

--- setup_R_Training.sh
+++ setup_R_Training.sh
@@ -1,7 +1,7 @@
 ## Setup R training op een DO machientje
 
 # login eerste
-ssh root@188.166.33.156
+ssh root@167.99.223.77
 
 # maak een user ruser met pasword ruser123 en zet ook in sudo group
 useradd -m ruser
@@ -9,20 +9,22 @@ passwd    ruser
 ruser123
 gpasswd -a ruser sudo
 
+su ruser
 
 ###### install R #####################################################################################
-sudo sh -c 'echo "deb http://cloud.r-project.org/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list' 
+sudo sh -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list' 
 ## add keys
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 ## install
 sudo apt-get update 
-sudo apt-get -y install r-base 
+sudo apt-get  install r-base 
+Y
 
 ####### install RStudio ###############################################################################
 sudo apt-get install gdebi-core
 wget https://download2.rstudio.org/rstudio-server-1.1.447-amd64.deb
-sudo --n gdebi rstudio-server-1.1.447-amd64.deb
+sudo gdebi --n rstudio-server-1.1.447-amd64.deb
 
 ### installeer dependencies om devtools te kunnen installeren
 sudo apt-get -y install libcurl4-gnutls-dev
